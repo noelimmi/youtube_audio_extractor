1b800d694f003fb5f78cfd841d67b5bfb1dfb7b9
noelimmi <noelimmanuel1998@gmail.com>
2019-11-03 22:52:19 +0530

    Handled wrong video id.

--- index.js
+++ index.js
@@ -5,31 +5,36 @@ const app = express();
 
 app.use(express.json());
 
-
 app.get('/getaudio/:yid', async (req, res) => {
     const videoID = req.params.yid;
     let result = new Array();
     ytdl.getInfo(videoID, (err, info) => {
-        if (err) throw err;
-        const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-        if (audioFormats.length > 0) {
-            audioFormats.forEach(audioItem => {
-                let temp = {};
-                temp['audioBitrate'] = audioItem.audioBitrate;
-                temp['url'] = audioItem.url;
-                result.push(temp);
-            });
-        }
-        if (result.length > 0) {
-            res.status(200).json({
-                status: true,
-                audio: result
+        if (err) {
+            res.status(400).json({
+                status: false,
+                errorMessage: "Invalid video id."
             });
         } else {
-            res.status(404).json({
-                status: false,
-                errorMessage: "Sorry,No audio found."
-            });
+            const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+            if (audioFormats.length > 0) {
+                audioFormats.forEach(audioItem => {
+                    let temp = {};
+                    temp['audioBitrate'] = audioItem.audioBitrate;
+                    temp['url'] = audioItem.url;
+                    result.push(temp);
+                });
+            }
+            if (result.length > 0) {
+                res.status(200).json({
+                    status: true,
+                    audio: result
+                });
+            } else {
+                res.status(404).json({
+                    status: false,
+                    errorMessage: "Sorry,No audio found."
+                });
+            }
         }
     });
 });
