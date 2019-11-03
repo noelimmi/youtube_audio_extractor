a5be882aaeea8b65302e74afd5b176966bdfb28a
noelimmi <noelimmanuel1998@gmail.com>
2019-11-03 19:49:14 +0530

    done audio extractor using ytdl-core

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/getaudio/:yid', async (req, res) => {
+    const videoID = req.params.yid;
+    let result = new Array();
+    ytdl.getInfo(videoID, (err, info) => {
+        if (err) throw err;
+        const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+        if (audioFormats.length > 0) {
+            audioFormats.forEach(audioItem => {
+                let temp = {};
+                temp['audioBitrate'] = audioItem.audioBitrate;
+                temp['url'] = audioItem.url;
+                result.push(temp);
+            });
+        }
+        if (result.length > 0) {
+            res.status(200).json({
+                status: true,
+                audio: result
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                errorMessage: "Sorry,No audio found."
+            });
+        }
+    });
+});
+
+const port = process.env.PORT || 7000;
+app.listen(port, () => console.log(`Server started on ${port}`));
