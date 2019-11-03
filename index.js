9f8a881b7c41e9ce5902938d61704c8fdeb81b32
noelimmi <noelimmanuel1998@gmail.com>
2019-11-04 02:57:56 +0530

    added download endpoint with index

--- index.js
+++ index.js
@@ -1,42 +1,97 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/getaudio/:yid', async (req, res) => {
-    const videoID = req.params.yid;
-    let result = new Array();
-    ytdl.getInfo(videoID, (err, info) => {
-        if (err) {
-            res.status(400).json({
-                status: false,
-                errorMessage: "Invalid video id."
-            });
-        } else {
-            const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-            if (audioFormats.length > 0) {
-                audioFormats.forEach(audioItem => {
-                    let temp = {};
-                    temp['audioBitrate'] = audioItem.audioBitrate;
-                    temp['url'] = audioItem.url;
-                    result.push(temp);
-                });
-            }
-            if (result.length > 0) {
-                res.status(200).json({
-                    status: true,
-                    audio: result
-                });
-            } else {
-                res.status(404).json({
-                    status: false,
-                    errorMessage: "Sorry,No audio found."
-                });
-            }
+app.get('', (req, res) => {
+    res.json({
+        to_get_audio_stream_list: {
+            method: "GET",
+            endpoint: "/api/audiostreamlist/:yid"
+        },
+        to_get_downloadable_audio: {
+            method: "GET",
+            endpoint: "/api/downloadaudio/:yid"
         }
     });
+});
+
+app.get('/api/audiostreamlist/:yid', (req, res) => {
+    const videoID = req.params.yid;
+    let result = new Array();
+    if (ytdl.validateID(videoID)) {
+        ytdl.getInfo(videoID, (err, info) => {
+            if (err) {
+                console.error(err);
+                res.status(404).json({
+                    status: false,
+                    errorMessage: "This audio is unavailable."
+                });
+            } else {
+                const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+                if (audioFormats.length > 0) {
+                    audioFormats.forEach(audioItem => {
+                        let temp = {};
+                        temp['audioBitrate'] = audioItem.audioBitrate;
+                        temp['url'] = audioItem.url;
+                        result.push(temp);
+                    });
+                    res.status(200).json({
+                        status: true,
+                        audio: result
+                    });
+                } else {
+                    res.status(404).json({
+                        status: false,
+                        errorMessage: "Sorry,No audio found."
+                    });
+                }
+            }
+        });
+    } else {
+        res.status(404).json({
+            status: false,
+            errorMessage: "This audio is unavailable."
+        });
+    }
+});
+
+app.get('/api/downloadaudio/:yid', (req, res) => {
+    const videoID = req.params.yid;
+    if (ytdl.validateID(videoID)) {
+        const stream = ytdl(videoID, {
+            quality: 'highestaudio',
+            filter: 'audioonly'
+        });
+        res.setHeader('Content-disposition', 'attachment; filename=' + videoID + '.mp3');
+        res.setHeader('Content-type', 'audio/mpeg');
+        ffmpeg({
+                source: stream
+            })
+            .setFfmpegPath(ffmpegPath)
+            .withAudioCodec('libmp3lame')
+            .toFormat('mp3')
+            .on('end', () => console.log('finished'))
+            .on('error', err => {
+                console.log(err.message);
+                res.status(500).json({
+                    status: false,
+                    errorMessage: err.message
+                });
+            })
+            .pipe(res, {
+                end: true
+            });
+    } else {
+        res.status(404).json({
+            status: false,
+            errorMessage: "This audio is unavailable."
+        });
+    }
 });
 
 const port = process.env.PORT || 7000;
